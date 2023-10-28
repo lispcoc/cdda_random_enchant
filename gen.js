65317e4f896e1528f952715382dcc764d50c7a02
LISPCoC <fourier0520@gmail.com>
2023-10-28 19:45:40 +0900

    update

--- gen.js
+++ gen.js
@@ -5,6 +5,7 @@ var list = [
     {"id": "PLANT_KILLER", "name": "植物特攻", "info": "このアイテムは<info>植物に追加ダメージ</info>を与えます。"},
     {"id": "INSECT_KILLER", "name": "虫特攻", "info": "このアイテムは<info>虫に追加ダメージ</info>を与えます。"},
     {"id": "FLIGHT_KILLER", "name": "飛行特攻", "info": "このアイテムは<info>飛んでいる敵に追加ダメージ</info>を与えます。"},
+    {"id": "BLEED", "name": "出血", "info": "このアイテムは攻撃が命中したときに<info>敵を出血</info>させます"},
     {"id": "DRAIN_HP", "name": "HP回復", "info": "このアイテムは攻撃が命中したときに<info>HPを回復</info>します。"},
     {"id": "DRAIN_MANA", "name": "マナ回復", "info": "このアイテムは攻撃が命中したときに<info>マナを回復</info>します。"},
     {"id": "DRAIN_STAMINA", "name": "スタミナ回復", "info": "このアイテムは攻撃が命中したときに<info>スタミナを回復</info>します。"},
@@ -26,6 +27,7 @@ list.forEach( e => {
         }
     ]}
     eoc.id = "EOC_random_enchant_lottery[" + e.id + "]"
+    var tmp = []
     for(var i = 1; i < 4; i++ ) {
         var j = {}
         j.id = "random_enchant_" + e.id + "_" + i
@@ -33,9 +35,11 @@ list.forEach( e => {
         j.info = "[<good>" + e.name + "Lv:" + i + "</good>]" + e.info
         json.push(j)
         eoc.effect[0].cases.push(
-            { "case": i, "effect": { "npc_set_flag": j.id } }
+            { "case": i, "effect": [{ "npc_set_flag": j.id }] }
         )
+        tmp.push(j.id)
     }
+    eoc["condition"] = {"not": {"or": tmp.map(id => {return {"npc_has_flag": id}}) }}
     eocs.push(eoc)
 })
 
@@ -57,5 +61,16 @@ var eff2 = json.map( e => {
 //    "effect": eff2
 //})
 
+var items_debug = list.map( e => {
+    return {
+        "id": "enchant_crystal_debug_[" + e.id + "]",
+        "type": "TOOL",
+        "copy-from": "enchant_crystal",
+        "name": { "str": "エンチャント・クリスタル(" + e.name + ")" },
+        "flags": [ "random_enchant_" + e.id + "_3"]
+      }
+})
+
 fs.writeFileSync("flags.json", JSON.stringify(json, null, "  "))
 fs.writeFileSync("lottery/flags_eoc.json", JSON.stringify(eocs, null, "  "))
+fs.writeFileSync("items_debug.json", JSON.stringify(items_debug, null, "  "))
